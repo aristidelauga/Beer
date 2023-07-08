f0a21ee873a01d5fa79e752cc9649892c6f3e9be
aristidelauga <aristidelauga@gmail.com>
2023-07-08 12:17:28 +0200

    Syncing to remote repository

---
+++ Beer/BeerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeerApp.swift
+//  Beer
+//
+//  Created by Aristide LAUGA on 08/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct BeerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
